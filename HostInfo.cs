c04fc5bb1956f0e566b15f5a083a7d94b1d9195d
sumx86 <ichi256@abv.bg>
2021-03-29 19:39:41 +0100

    Update HostInfo.cs

--- HostInfo.cs
+++ HostInfo.cs
@@ -1,32 +1,63 @@
-﻿using System;
+using System;
 using System.Text;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace ConsoleApp2.HostInfo
 {
     class HostInfo
     {
-        private static StringBuilder info = new StringBuilder();
+        private static StringBuilder _info = new StringBuilder();
+        private static StringBuilder _HWid;
+
         public HostInfo()
         {
             var os = Environment.OSVersion;
-            info.Append("Platform: "        + os.Platform                 + "\n");
-            info.Append("Version String: "  + os.VersionString            + "\n");
-            info.Append("ServicePack: "     + os.ServicePack              + "\n");
-            info.Append("User Name: "       + Environment.UserName        + "\n");
-            info.Append("SystemDirectory: " + Environment.SystemDirectory + "\n");
+            _info.Append("Platform: "        + os.Platform                 + "\n");
+            _info.Append("Version String: "  + os.VersionString            + "\n");
+            _info.Append("ServicePack: "     + os.ServicePack              + "\n");
+            _info.Append("User Name: "       + Environment.UserName        + "\n");
+            _info.Append("SystemDirectory: " + Environment.SystemDirectory + "\n");
 
             string ip = Utils.GetIP();
             if( ip == "" ) {
                 ip = "NO-IP";
             }
-            info.Append( "Ip Address: " + ip + "\n" );
+            _info.Append( "Ip Address: " + ip + "\n" );
+            _HWid = ComputeHWid();
+        }
+
+        private static StringBuilder ComputeHWid()
+        {
+            return ComputeHash(
+                Environment.ProcessorCount + Environment.UserName +
+                Environment.MachineName + Environment.OSVersion + new DriveInfo(Path.GetPathRoot(Environment.SystemDirectory)).TotalSize);
+        }
+
+        private static StringBuilder ComputeHash(string str)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] hashBytes = md5.ComputeHash(Encoding.ASCII.GetBytes(str));
+            StringBuilder hashValue = new StringBuilder();
+            foreach (byte b in hashBytes) {
+                hashValue.Append(b.ToString("x2"));
+            }
+            return hashValue;
         }
 
         public StringBuilder Info
         {
             get
             {
-                return info;
+                return _info;
+            }
+        }
+
+        public StringBuilder Hwid
+        {
+            get
+            {
+                return _HWid;
             }
         }
     }
