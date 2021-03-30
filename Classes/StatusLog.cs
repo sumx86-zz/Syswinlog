4be6041f8145710476ef5796de8ccf3d2cfa12ce
sumx86 <ichi256@abv.bg>
2021-03-30 17:30:31 +0100

    Update StatusLog.cs

--- Classes/StatusLog.cs
+++ Classes/StatusLog.cs
@@ -1,10 +1,19 @@
-﻿using System;
+using System;
 using System.Text;
+using System.IO;
 
 namespace Syswinlog.Classes.StatusLog
 {
     class StatusLog
     {
+        private const string _logFile = @"/SysWinlog32/logs/status.txt";
 
+        public static void Log(string message)
+        {
+            string date = DateTime.Now.ToString("dd/MMMM/yyyy - HH:mm:ss tt");
+            using (StreamWriter sw = new StreamWriter(_logFile)) {
+                sw.Write(date + " -- " + message);
+            }
+        }
     }
 }
